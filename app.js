d652059755ab940429e9c1cc29eecb090d3498c7
gavvahar <nihargavva@gmail.com>
2022-06-25 16:08:11 -0400

    still working on this

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 class Casino {
-    // Write code here
-};
+  // Write code here
+  constructor(name, isFakeCoin) {
+    this.name = name;
+    this.timesPlayed = 0;
+  }
+
+  playGame(betAmount) {
+    if (Math.random() <= 0.5 || this.isFakeCoin) {
+      console.log(`Casino ${this.name} won!`);
+    } else {
+      this.timesPlayed + 1;
+      console.log(`You have won $${betAmount * (this.timesPlayed + 1)}`);
+    }
+  }
+}
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
@@ -11,20 +24,16 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
 
 // Extra BONUS TESTS
-/*
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
